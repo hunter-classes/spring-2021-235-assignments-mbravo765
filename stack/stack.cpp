41d654f514d5ed67d169c58d75ebd80250f1837a
Michelle Bravo <bravomichelle292@gmail.com>
2021-03-24 21:54:01 -0400

    fixed order

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include "stack.h"
-#include <vector>
-
 
 Stack::Stack()
 {
@@ -11,21 +9,8 @@ Stack::Stack()
 void Stack::push(int i)
 {
 //push is used to push or insert an element at the top of the stack container.
-		v.insert(v.begin(), i);
+		v.push_back(i);
 		size++;
-}
-
-bool Stack::isEmpty()
-{
-//returns truee if stack is empty, else false
-	if(size > 0)
-	{
-		return false;
-	}
-	else
-	{
-		return true;
-	}
 }
 
 void Stack::pop()
@@ -47,14 +32,26 @@ void Stack::pop()
 void Stack::top()
 {
 //returns the top element of the stack
-	if(size < 0)
-	{
-		std::cout << "Stack is empty";
-	}
-	else
+	if(size > 0)
 	{
 		int x = v[size-1];
 		std::cout << x << "\n";
+	}
+	else
+	{
+		std::cout << "stack is empty" << "\n";
+	}
+}
+
+bool Stack::isEmpty()
+{
+	if(size > 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
 	}
 }
 
@@ -62,7 +59,7 @@ void Stack::toString()
 {
 	for(int i=0; i<size; i++)
 	{
-		std::cout << v[i] << " ";
+		std::cout << v.at(i) << " ";
 	}
 	std::cout << "\n";
 }
