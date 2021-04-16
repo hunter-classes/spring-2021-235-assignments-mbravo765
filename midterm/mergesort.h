2654dc1709a37dc33dfccfcc83709139f1d090fe
Michelle Bravo <bravomichelle292@gmail.com>
2021-04-15 20:53:23 -0400

    h file

---
+++ midterm/mergesort.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Node.h"
+
+int length(Node *l);
+Node *middle(Node *head);
+Node *merge(Node *a, Node *b);
+Node *msort(Node *head);
+std::string toString(Node *head);
+
+
+
